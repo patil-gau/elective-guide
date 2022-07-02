71d2f421d0c28bc14921a652812b4da16bac217c
patil-gau <patilgautam728@gmail.com>
2022-07-02 18:07:04 +0530

    basic project structure created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
 import { NativeBaseProvider, extendTheme } from "native-base";
 import reportWebVitals from "./reportWebVitals";
+import { createRoot } from 'react-dom/client';
 
 const theme = extendTheme({
   config: {
@@ -10,14 +10,17 @@ const theme = extendTheme({
   },
 });
 
-ReactDOM.render(
-  <React.StrictMode>
-    <NativeBaseProvider theme={theme}>
-      <App />
-    </NativeBaseProvider>
-  </React.StrictMode>,
+const root = createRoot(
   document.getElementById("root")
 );
+
+root.render(
+<React.StrictMode>
+  <NativeBaseProvider theme={theme}>
+    <App />
+  </NativeBaseProvider>
+</React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
